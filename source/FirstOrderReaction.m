161c72aa5b707e5cc2b956ecbd1320c868540b60
Austin Ladshaw <austin.ladshaw@span.io>
2023-11-02 13:48:08 -0700

    Saving work

---
+++ source/FirstOrderReaction.m
@@ -0,0 +1,31 @@
+classdef FirstOrderReaction < PhysicsResidual
+    properties (Access = protected)
+        k   % Reaction coefficient
+    end
+    
+    methods
+        function obj = FirstOrderReaction(lb, ub, val)
+            arguments
+                lb (1,:) {mustBeNumeric} = 0
+                ub (1,:) {mustBeNumeric} = 1
+                val (1,1) {mustBePositive} = 1
+            end
+            obj = obj@PhysicsResidual(lb, ub);
+            obj.k = val;
+        end
+        
+        % bs_rhs -> j
+        % bs_lhs -> i
+        % return dR/dcj of diffusion
+        %       = k * sum(i,  c_i * phi_j * phi_i)
+        function res = eval(obj, bs_rhs, bs_lhs, c_i)
+            arguments
+                obj (1,1) {mustBeA(obj,'FirstOrderReaction')}
+                bs_rhs (1,1) {mustBeA(bs_rhs,'BasisSet')}
+                bs_lhs (1,1) {mustBeA(bs_lhs,'BasisSet')}
+                c_i (:,1) {mustBeNumeric}
+            end
+            res = obj.k .* (integral_x_phi(bs_rhs, bs_lhs, obj.lb, obj.ub) * c_i);
+        end
+    end
+end
---
+++ tests/test_ReactionDiffusion.m
@@ -0,0 +1,60 @@
+% Now have the minimum of what is technically required to build and solve
+% a Reaction diffusion equation...
+
+% Solve:  -D * d2u/dx2 + k*u = 0   with u(0) = uo and du(L) = 0
+%
+%       Basis set: Monomial1D, with basis size 3 and 2 BCs
+
+vset = Variables();
+u = Monomial1D(3);
+vset.add_variable(u);
+
+vset.set_all_coefficients([0;0;0]);
+c = vset.get_variable_coefficients('u');
+% temporary storage of Lagrangian multipliers
+l = [0;0];
+
+D = 0.5;
+k = 2;
+L = 1;
+uo = 1;
+diff = Diffusion(0,L,D);
+react = FirstOrderReaction(0,L,k);
+
+bc1 = NeumannBC(0);
+bc1_r = L;
+bc2 = DirichletBC(uo);
+bc2_r = 0;
+
+x_span = 0:(L/10):L;
+u_exact = uo*(exp((2*L-x_span)*sqrt(k/D))+exp(x_span*sqrt(k/D)))./(1+exp(2*L*sqrt(k/D)));
+
+funop = @(x) diffreact(x,vset,diff,react,bc1,bc1_r,bc2,bc2_r);
+
+% Alternative using fminsearch 
+x = fminsearch(funop,[0;0;0;0;0]);
+
+
+
+% Helper function for residuals
+function Ax = diffreact(x,vset,diff,react,bc1,bc1_r,bc2,bc2_r)
+    c1 = x(1);
+    c2 = x(2);
+    c3 = x(3);
+    vset.set_all_coefficients([c1;c2;c3]);
+    l1 = x(4);
+    l2 = x(5);
+
+    u = vset.get_variable('u');
+
+    Ax = zeros(5,1);
+    Ax(1:3,1) = diff.eval(u,u,[c1;c2;c3]) + ...
+                    react.eval(u,u,[c1;c2;c3]) + ...
+                    bc1.eval_dLdc(u,[c1;c2;c3],bc1_r,l1) + ...
+                    bc2.eval_dLdc(u,[c1;c2;c3],bc2_r,l2);
+    Ax(4,1) = bc1.eval_dLdl(u,[c1;c2;c3],bc1_r,l1);
+    Ax(5,1) = bc2.eval_dLdl(u,[c1;c2;c3],bc2_r,l2);
+
+    % Force a norm calculation (for fminsearch)
+    Ax=norm(Ax);
+end
